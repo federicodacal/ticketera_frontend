90b7f8a16455b66d9ce46c24e597a27105c12d70
Federico Dacal <federicodacal@gmail.com>
2023-11-30 21:41:13 -0300

    frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ticketera_app';
+
+  readonly url =  'https://ticketera-mongodb-api.vercel.app/api/';
+
+  data:any = [];
+
+  constructor(private http:HttpClient) { }
+
+  ngOnInit() {
+    //this.getTickets();
+    this.getUsers();
+  }
+
+  getTickets() {
+    this.http.get(this.url+'tickets').subscribe((res:any) => {
+      console.log(res.tickets);
+      this.data = res.tickets;
+    });
+  }
+
+  getUsers() {
+    this.http.get(this.url+'usuarios').subscribe((res:any) => {
+      console.log(res.users);
+      this.data = res.users;
+    });
+  }
+
 }
